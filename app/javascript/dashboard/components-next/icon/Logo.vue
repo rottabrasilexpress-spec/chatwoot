78cfec745dd06bfae581fb33616a5d01b890d0bb
Kelvin Martins <kelvinmartinsp@hotmail.com>
2026-09-09 13:01:39 -0300

    fix(rotta): prefer brand mark over official thumbnail

--- app/javascript/dashboard/components-next/icon/Logo.vue
+++ app/javascript/dashboard/components-next/icon/Logo.vue
@@ -1,18 +1,20 @@
 <script setup>
-import { useAttrs } from 'vue';
+import { computed, useAttrs } from 'vue';
 import { useMapGetter } from 'dashboard/composables/store';
 
 const attrs = useAttrs();
 const globalConfig = useMapGetter('globalConfig/get');
 const ROTTA_MARK_URL = '/brand-assets/rottabrasil-mark-transparent.png';
+const configuredLogo = computed(() => globalConfig.value?.logoThumbnail || '');
+const hasCustomConfiguredLogo = computed(
+  () =>
+    Boolean(configuredLogo.value) &&
+    !String(configuredLogo.value).includes('/logo_thumbnail.svg')
+);
 </script>
 
 <template>
-  <img
-    v-if="globalConfig.logoThumbnail"
-    v-bind="attrs"
-    :src="globalConfig.logoThumbnail"
-  />
+  <img v-if="hasCustomConfiguredLogo" v-bind="attrs" :src="configuredLogo" />
   <img
     v-else
     v-bind="attrs"
