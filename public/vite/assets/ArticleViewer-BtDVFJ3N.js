fadc8f4d3eb86f173b49ca1fe14fd79ffb8e3480
Kelvin Martins <kelvinmartinsp@hotmail.com>
2026-09-12 02:04:33 -0300

    build(chatwoot): include manifest runtime chunks

---
+++ public/vite/assets/ArticleViewer-BtDVFJ3N.js
@@ -0,0 +1,2 @@
+import{_ as r}from"./IframeLoader-B6b1Kz6B.js";import{_ as t,j as n,o as s,e as a,k as c}from"./_plugin-vue_export-helper-PM2umi06.js";import"./useBranding-DN7wx2q8.js";import"./utils.esm-DY_uR2pP.js";const i={name:"ArticleViewer",components:{IframeLoader:r}},l={class:"bg-n-solid-1 h-full"};function m(e,_,p,f,d,u){const o=n("IframeLoader");return s(),a("div",l,[c(o,{url:e.$route.query.link},null,8,["url"])])}const L=t(i,[["render",m]]);export{L as default};
+//# sourceMappingURL=ArticleViewer-BtDVFJ3N.js.map
